9971207b804eaa1bdf018cd412a5b058633ad461
Margaret Blauvelt <margaret@blauvelt.com>
2018-03-02 17:37:01 -0800

    Add event handler for leaving the common name field (on focusout)

--- public/app.js
+++ public/app.js
@@ -1,1 +1,23 @@
 "use strict";
+//
+function handleClicks() {
+  $("#js-common-name").on("focusout", function(e) {
+    e.preventDefault();
+    let commonName = $("#js-common-name").val();
+    console.log("common name:");
+    console.log(commonName);
+    if (commonName.length > 1) {
+      $("#js-scientific-name").val(lookupScientificName(commonName));
+    }
+  });
+}
+
+function lookupScientificName(commonName) {
+  console.log("Look up scientific name for " + commonName);
+  return "Some scientific name for " + commonName;
+}
+
+$(document).ready(function() {
+  console.log("You are running app.js in Bird Logger");
+  handleClicks();
+});
