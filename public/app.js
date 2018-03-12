b60481088dc62c885bcf9c0e5e2d606f812905c4
Margaret Blauvelt <margaret@blauvelt.com>
2018-03-11 18:40:04 -0700

    Refactor to use generic API calling function.

--- public/app.js
+++ public/app.js
@@ -1,22 +1,33 @@
 "use strict";
-//
-function handleClicks() {
-  $("#js-common-name").on("focusout", function(e) {
-    e.preventDefault();
-    let commonName = $("#js-common-name").val();
-    if (commonName.length > 1) {
-      const ApiLookupResult = lookupScientificNames(commonName);
-    }
-  });
+
+function getDataFromApi(baseUrl, searchTerm, callback) {
+  const settings = {
+    url: baseUrl + searchTerm,
+    dataType: "json",
+    type: "GET",
+    success: callback
+  };
+
+  $.ajax(settings);
 }
 
+/**
+ * Look up similar common names and Taxonomic Serial Numbers (TSNs) at ITIS from common name input by user
+ * @method lookupScientificNames
+ * @param {string} commonName - Common name for bird
+ * @return result
+ */
 function lookupScientificNames(commonName) {
   console.log(`Look up scientific name for "${commonName}"`);
-  const settings = {};
-  var result = $.getJSON("https://www.itis.gov/ITISWebService/jsonservice/searchByCommonName?srchKey=" + commonName, settings, parseTSNs);
-  return result;
+  getDataFromApi("https://www.itis.gov/ITISWebService/jsonservice/searchByCommonName?srchKey=", commonName, parseTSNs);
 }
 
+/**
+ * Parse the JSON and store similar common names and TSNs in "namesTsns" array
+ * @method parseTSNs
+ * @param {object} data - JSON returned by API call in lookupScientificNames
+ * @return
+ */
 function parseTSNs(data) {
   if (data.commonNames.length === 1 && !data.commonNames[0]) {
     console.log(`No matching common names were found.`);
@@ -31,45 +42,69 @@ function parseTSNs(data) {
 
   console.log(`Number of entries returned: ${data.commonNames.length}`);
   console.log(namesTsns);
-  const tsns = [];
-  $.each(namesTsns, function(dataKey, dataValue) {
-    // console.log("TSN: " + dataValue.tsn);
-    tsns.push(dataValue.tsn);
-  });
-  console.log(tsns);
-  const birdScientificNames = findScientificNameFromTsn(tsns);
+  const animalResults = findScientificNameFromTsn(namesTsns);
   $("#js-scientific-name").val("Scientific name goes here");
   return;
 }
 
-function findScientificNameFromTsn(tsns) {
+/**
+ * For each organism in array, send TSN to getOrganism function to look up details.
+ * @method findScientificNameFromTsn
+ * @param {} namesTsns
+ * @return
+ */
+function findScientificNameFromTsn(namesTsns) {
   // https://www.itis.gov/ITISWebService/jsonservice/getScientificNameFromTSN?tsn=179759 // Robin
-  const animals = [];
-  for (let i = 0; i < tsns.length; i++) {
-    // for (let i = 0; i < 1; i++) {
-    const res = getOrganism(tsns[i]);
+  for (let i = 0; i < namesTsns.length; i++) {
+    // const res = getOrganism(namesTsns[i].tsn);
+    getDataFromApi("https://www.itis.gov/ITISWebService/jsonservice/getScientificNameFromTSN?tsn=", namesTsns[i].tsn, parseOrganism);
   }
+}
 
-  function getOrganism(tsn) {
-    // let tsn = 179759; // Test with Robin
-    const settings = {};
-    var result = $.getJSON("https://www.itis.gov/ITISWebService/jsonservice/getScientificNameFromTSN?tsn=" + tsn, settings, parseScientificNames);
-    return result;
+/**
+ * Extract kingdom and scientific name from API result
+ * @method parseOrganism
+ * @param {} data
+ * @return
+ */
+function parseOrganism(data) {
+  const animal = [];
+  if (data.kingdom.toLowerCase() === "animalia") {
+    animal.push({ kingdom: data.kingdom, tsn: data.tsn, scientificName: data.combinedName });
+    console.log("animal in parseOrganism");
+    console.log(animal);
+    getDataFromApi("https://www.itis.gov/ITISWebService/jsonservice/getParentTSNFromTSN?tsn=", data.tsn, parseParent);
+    // return animal;
   }
+}
 
-  function parseScientificNames(data) {
-    const sciNamesTsns = [];
-    console.log("parseScientificNames");
-    // console.log(data);
-    if (data.kingdom.toLowerCase() === "animalia") {
-      animals.push({ kingdom: data.kingdom, tsn: data.tsn, scientificName: data.combinedName });
-      return;
+/**
+ * Extract relevant TSN parent data
+ * @method parseParent
+ * @param {} data
+ * @return
+ */
+function parseParent(data) {
+  console.log("Inside parseParent");
+  console.log(data);
+}
+
+/**
+ * Handle user events
+ * @method handleUserActions
+ * @return
+ */
+function handleUserActions() {
+  $("#js-common-name").on("focusout", function(e) {
+    e.preventDefault();
+    let commonName = $("#js-common-name").val();
+    if (commonName.length > 1) {
+      const ApiLookupResult = lookupScientificNames(commonName);
     }
-  }
-  console.log(animals);
+  });
 }
 
 $(document).ready(function() {
   // console.log("You are running app.js in Bird Logger");
-  handleClicks();
+  handleUserActions();
 });
